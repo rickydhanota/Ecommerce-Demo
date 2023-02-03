186f0ff8054dd6cb12d12e0df4ce5c2383f74672
Ricky <rickdhanota@gmail.com>
2023-02-02 23:31:03 -0800

    more boilerplate setup

---
+++ server/server.js
@@ -0,0 +1,44 @@
+// import express from "express";
+// import bodyParser, { urlencoded } from "body-parser";
+// import mongoose from "mongoose";
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import helmet from "helmet";
+// import morgan from "morgan";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(morgan("common"));
+
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// MONGOOSE SETUP
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) =>
+    console.log("Something went wrong in the mongoose file", err)
+  );

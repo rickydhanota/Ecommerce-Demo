853aa963d2e440f3f674bfe8d5e66a8fa1da0267
Ricky <rickdhanota@gmail.com>
2023-02-04 01:19:39 -0800

    Setting up redux and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
 // import './App.css';
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.global.mode);
+  console.log(mode);
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />} path="/" />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

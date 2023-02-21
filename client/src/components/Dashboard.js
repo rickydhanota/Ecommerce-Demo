1ed074f6e9b05a669fb0f0b777af6059f12a12c1
Ricky Dhanota <rickydhanota@Rickys-MacBook-Pro.local>
2023-02-21 00:55:44 -0800

    Testing after cloning

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Dashboard = (props) => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <h1>This is testing after I've cloned repo</h1>
+    </div>
+  );
 };
 
 export default Dashboard;
